7dfc2f802f04edbd70530b592e062227ffbab0c5
AnFav1 <farfanangiz10@autonoma.edu.pe>
2025-07-10 04:26:56 -0500

    Cambios nuevos

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
